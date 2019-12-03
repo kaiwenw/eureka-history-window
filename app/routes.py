022754652805805290a251d7b99a0b20ecc9021a
wangkaiwen998@gmail.com <wangkaiwen998@gmail.com>
2019-12-02 21:05:28 -0500

    wip

--- app/routes.py
+++ app/routes.py
@@ -18,9 +18,10 @@ def reset_states():
     app.config["CURRENT_SESSION_NAME"] = None
 
 
-def invalid_session():
+def inconsistent_state():
     return (
         app.config["LOG_FOLDER"] is None or app.config["CURRENT_SESSION_NAME"] is None
+        or not os.path.isdir(app.config["LOG_FOLDER"])
     )
 
 
@@ -39,6 +40,7 @@ def update_log_folder(session_name):
 
 def get_stat_series(rows):
     """Helper that formats statistics into a list-form for D3 plot to consume"""
+    assert rows is not None
     stat_series = []
     # at the end of each row, add a zero point
     for row in rows:
@@ -96,45 +98,57 @@ def download_predicate(sess_num):
 def homepage():
     """Loads the main page, with tables and plots, and buttons to replay"""
     candidates = get_valid_session_paths(app.config["ROOT_FOLDER"])
+    # nothing to show, since no valid candidate folder
     if len(candidates) == 0:
+        print("No candidates!")
         return render_template("index.html")
 
-    print("candidates: ", candidates)
-    if invalid_session() or app.config["CURRENT_SESSION_NAME"] not in candidates:
+    # make current state consistent (can become inconsistent after this, but let's assume not)
+    if inconsistent_state() or app.config["CURRENT_SESSION_NAME"] not in candidates:
         update_log_folder(candidates[0])
 
-    rows = None
-    stat_keys = None
-    if app.config["LOG_FOLDER"] and os.path.isdir(app.config["LOG_FOLDER"]):
-        rows = process_data(app.config["LOG_FOLDER"])
+    rows = process_data(app.config["LOG_FOLDER"])
+    # state is consistent, but folder is malformed :(
+    if rows is None:
+        print("invalid rows from log folder: ", app.config["LOG_FOLDER"])
+        return render_template(
+                "index.html",
+                session_name=app.config["CURRENT_SESSION_NAME"],
+                candidates=candidates,
+            )
 
-    if rows is not None and len(rows) > 0 and len(rows[0]["per_img"]) > 0:
-        stat_keys = rows[0]["per_img"][0]["derived_stats"].keys()
-
+    stat_keys = rows[0]["per_img"][0]["derived_stats"].keys()
     return render_template(
         "index.html",
         session_name=app.config["CURRENT_SESSION_NAME"],
-        candidate_logs=candidates,
+        candidates=candidates,
         rows=rows,
         stat_keys=stat_keys,
     )
+
+####################################
+########## REPLAY WINDOW ###########
+####################################
 
 
 @app.route("/replay/<int:sess_num>")
 def replay_homepage(sess_num):
     """Homepage for replay, which sets up the <img> tags to make
-    GET request to replay_img, and also passes the labels
+    GET request to replay_img, and also passes the labels.
+    Note, we assume that in between this function call and the GET requests,
+    that the state remains consistent. This may not be the case, as the user
+    can maliciously delete files in this time frame. We assume this isn't the case.
     """
-    if app.config["LOG_FOLDER"] is None:
-        print("No log folder, defaulting to index")
+    if inconsistent_state():
+        print("inconsistent state, returning to homepage")
         return homepage()
 
     rows = process_data(app.config["LOG_FOLDER"])
-    per_img = rows[sess_num]["per_img"]
-    pos_ids = rows[sess_num]["positive_ids"]
-    neg_ids = rows[sess_num]["negative_ids"]
     return render_template(
-        "replay.html", per_img=per_img, pos_ids=pos_ids, neg_ids=neg_ids
+        "replay.html",
+        per_img=rows[sess_num]["per_img"],
+        pos_ids=rows[sess_num]["positive_ids"],
+        neg_ids=rows[sess_num]["negative_ids"],
     )
 
 
