2362479793aa99d4a2d632c23aecee68519939a3
wangkaiwen998@gmail.com <wangkaiwen998@gmail.com>
2019-12-02 19:29:22 -0500

    black

--- app/logs.py
+++ app/logs.py
@@ -7,74 +7,81 @@ from collections import OrderedDict
 def get_sorted_num_files(folder):
     if not os.path.isdir(folder):
         return []
-    filtered_folder = filter(lambda p: p.split('.')[0].isdigit(), os.listdir(folder))
-    mapped_folder = map(lambda x: int(x.split('.')[0]), filtered_folder)
+    filtered_folder = filter(lambda p: p.split(".")[0].isdigit(), os.listdir(folder))
+    mapped_folder = map(lambda x: int(x.split(".")[0]), filtered_folder)
     return sorted(mapped_folder)
 
+
 def get_sorted_subfolders(folder):
-    assert(os.path.isdir(folder))
+    assert os.path.isdir(folder)
     filtered_folder = filter(lambda p: p.isdigit(), os.listdir(folder))
     mapped_folder = map(lambda x: int(x), filtered_folder)
     return sorted(mapped_folder)
 
+
 # very preliminary search for some potentially viable paths
-def get_all_files(root_folder, search_from_path, depth = 2):
-    assert(depth >= 0)
-    if (not os.path.isdir(search_from_path)):
+def get_all_files(root_folder, search_from_path, depth=2):
+    assert depth >= 0
+    if not os.path.isdir(search_from_path):
         return []
     res = []
     for file in os.listdir(search_from_path):
         new_path = os.path.join(search_from_path, file)
-        if (not os.path.isdir(new_path)):   continue
+        if not os.path.isdir(new_path):
+            continue
         # it is a candidate if it has a direct subfolder of 0, 1, 2...
         sorted_subfolders = get_sorted_subfolders(new_path)
-        if (len(sorted_subfolders) > 0):
+        if len(sorted_subfolders) > 0:
             good = True
             for i in range(len(sorted_subfolders)):
                 if not (i in sorted_subfolders):
                     good = False
                     print(i, "not in", sorted_subfolders)
                     break
-            if (good):
-                assert(new_path[:len(root_folder)] == root_folder)
-                rest = new_path[len(root_folder):]
+            if good:
+                assert new_path[: len(root_folder)] == root_folder
+                rest = new_path[len(root_folder) :]
                 print(sorted_subfolders, "good", new_path, rest)
                 res.append(rest)
 
-        if (depth >= 1):
-            res.extend(get_all_files(root_folder, new_path, depth-1))
+        if depth >= 1:
+            res.extend(get_all_files(root_folder, new_path, depth - 1))
     return res
 
-'''
+
+"""
 Return a mapping of absolute time (ms) to an OrderedDict of
 - id
 - label
 while verifying that the times are indeed sequential
-'''
+"""
+
+
 def load_feedbacks_csv(csv_path):
     result = OrderedDict()
     if os.path.isfile(csv_path):
         last_time = None
         for idx, row in pandas.read_csv(csv_path).iterrows():
             entry = OrderedDict()
-            entry['id'] = row['id']
-            entry['label'] = row['feedback_label']
-            cur_time = int(row['absolute time(ms)'])
+            entry["id"] = row["id"]
+            entry["label"] = row["feedback_label"]
+            cur_time = int(row["absolute time(ms)"])
             result[cur_time] = entry
-            if (last_time is None):
+            if last_time is None:
                 last_time = cur_time
             else:
-                assert(last_time < cur_time)
+                assert last_time < cur_time
                 last_time = cur_time
     return result
 
+
 def float_div(num, denom):
-    if (int(denom) == 0):
+    if int(denom) == 0:
         return 0.0
     return float(num) / float(denom)
 
 
-'''
+"""
 Get all the metadata given the root_dir.
 Expects the following directory tree with S sessions
 root_dir
@@ -128,7 +135,9 @@ There is one more entry for the end, where it only has the derived_stats field.
 
 The implementation will try to be as faithful to the order as possible.
 On error, will return None
-'''
+"""
+
+
 def process_data(root_dir):
     print("Processing data from %s" % root_dir)
     try:
@@ -139,22 +148,24 @@ def process_data(root_dir):
             row = OrderedDict()
             # predicate path
             pred_path = os.path.join(sess_dir, "pred.hyperfindsearch")
-            row['predicate_path'] = pred_path
-            with open(pred_path, 'r') as f:
-                row['predicates'] = json.load(f)
+            row["predicate_path"] = pred_path
+            with open(pred_path, "r") as f:
+                row["predicates"] = json.load(f)
 
             # read the info.json file
-            start_info_path = os.path.join(sess_dir, 'start_info.json')
-            with open(start_info_path, 'r') as f:
-                row['start_info'] = OrderedDict(json.load(f))
+            start_info_path = os.path.join(sess_dir, "start_info.json")
+            with open(start_info_path, "r") as f:
+                row["start_info"] = OrderedDict(json.load(f))
 
-            sess_start_time = int(row['start_info']['start_time(ms)'])
+            sess_start_time = int(row["start_info"]["start_time(ms)"])
 
-            row['derived_info'] = OrderedDict()
-            row['derived_info']['start_time'] = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(sess_start_time/1000))
+            row["derived_info"] = OrderedDict()
+            row["derived_info"]["start_time"] = time.strftime(
+                "%Y-%m-%d %H:%M:%S", time.localtime(sess_start_time / 1000)
+            )
 
             # temporary feedback mapping of time -> label + id
-            feedback_path = os.path.join(sess_dir, 'feedback.csv')
+            feedback_path = os.path.join(sess_dir, "feedback.csv")
             time2feedback = load_feedbacks_csv(feedback_path)
             cur_feedback_idx = 0
 
@@ -167,34 +178,46 @@ def process_data(root_dir):
                 nonlocal pos_feedback_set, neg_feedback_set
                 feedback_keys = list(time2feedback.keys())
                 # first, move the feedback statistics forward to the current time
-                while (cur_feedback_idx < len(feedback_keys) and feedback_keys[cur_feedback_idx] <= cur_img_time):
+                while (
+                    cur_feedback_idx < len(feedback_keys)
+                    and feedback_keys[cur_feedback_idx] <= cur_img_time
+                ):
                     cur_feedback = time2feedback[feedback_keys[cur_feedback_idx]]
-                    if cur_feedback['label'] == 'Positive':
-                        pos_feedback_set = pos_feedback_set | {cur_feedback['id']}
-                        neg_feedback_set = neg_feedback_set - {cur_feedback['id']}
-                    elif cur_feedback['label'] == 'Negative':
-                        pos_feedback_set = pos_feedback_set - {cur_feedback['id']}
-                        neg_feedback_set = neg_feedback_set | {cur_feedback['id']}
+                    if cur_feedback["label"] == "Positive":
+                        pos_feedback_set = pos_feedback_set | {cur_feedback["id"]}
+                        neg_feedback_set = neg_feedback_set - {cur_feedback["id"]}
+                    elif cur_feedback["label"] == "Negative":
+                        pos_feedback_set = pos_feedback_set - {cur_feedback["id"]}
+                        neg_feedback_set = neg_feedback_set | {cur_feedback["id"]}
                     else:
-                        assert(cur_feedback['label'] == 'Ignore')
-                        pos_feedback_set = pos_feedback_set - {cur_feedback['id']}
-                        neg_feedback_set = neg_feedback_set - {cur_feedback['id']}
+                        assert cur_feedback["label"] == "Ignore"
+                        pos_feedback_set = pos_feedback_set - {cur_feedback["id"]}
+                        neg_feedback_set = neg_feedback_set - {cur_feedback["id"]}
                     cur_feedback_idx += 1
 
                 # second calculate the derived stats
                 derived = OrderedDict()
-                derived['items_processed'] = int(stats['Searched'])
-                derived['items_shown'] = int(stats['Passed'])
-                derived['new_hits'] = len(pos_feedback_set)
-                derived['pass_rate(%)'] = float_div(derived['items_shown'], derived['items_processed']) * 100.0
-                derived['precision(%)'] = float_div(derived['new_hits'], derived['items_shown']) * 100.0
-                derived['elapsed_time(min)'] = float(cur_img_time - sess_start_time) / 1000.0 / 60.
-                derived['productivity'] = float_div(derived['new_hits'], derived['elapsed_time(min)'])
+                derived["items_processed"] = int(stats["Searched"])
+                derived["items_shown"] = int(stats["Passed"])
+                derived["new_hits"] = len(pos_feedback_set)
+                derived["pass_rate(%)"] = (
+                    float_div(derived["items_shown"], derived["items_processed"])
+                    * 100.0
+                )
+                derived["precision(%)"] = (
+                    float_div(derived["new_hits"], derived["items_shown"]) * 100.0
+                )
+                derived["elapsed_time(min)"] = (
+                    float(cur_img_time - sess_start_time) / 1000.0 / 60.0
+                )
+                derived["productivity"] = float_div(
+                    derived["new_hits"], derived["elapsed_time(min)"]
+                )
                 return derived
 
-            meta_dir = os.path.join(sess_dir, 'metadata')
-            stat_dir = os.path.join(sess_dir, 'stats')
-            img_dir = os.path.join(sess_dir, 'thumbnail')
+            meta_dir = os.path.join(sess_dir, "metadata")
+            stat_dir = os.path.join(sess_dir, "stats")
+            img_dir = os.path.join(sess_dir, "thumbnail")
 
             per_img = []
             for img_num in get_sorted_num_files(stat_dir):
@@ -202,40 +225,46 @@ def process_data(root_dir):
 
                 # first load in metadata, stats and img path
                 meta_path = os.path.join(meta_dir, "%d.json" % img_num)
-                with open(meta_path, 'r') as f:
-                    cur_img['metadata'] = OrderedDict(json.load(f))
+                with open(meta_path, "r") as f:
+                    cur_img["metadata"] = OrderedDict(json.load(f))
 
                 stat_path = os.path.join(stat_dir, "%d.json" % img_num)
-                with open(stat_path, 'r') as f:
-                    cur_img['stats'] = OrderedDict(json.load(f))
+                with open(stat_path, "r") as f:
+                    cur_img["stats"] = OrderedDict(json.load(f))
 
                 # relative image page
-                cur_img['img_path'] = "%d/thumbnail/%d.jpeg" % (sess_num, img_num)
+                cur_img["img_path"] = "%d/thumbnail/%d.jpeg" % (sess_num, img_num)
 
-                cur_img['derived_stats'] = calc_derived_stats(cur_img['stats'], int(cur_img['metadata']['arrival_time(ms)']))
+                cur_img["derived_stats"] = calc_derived_stats(
+                    cur_img["stats"], int(cur_img["metadata"]["arrival_time(ms)"])
+                )
                 per_img.append(cur_img)
 
-            end_info_path = os.path.join(sess_dir, 'end_info.json')
-            assert(cur_feedback_idx <= len(time2feedback))
-            if (os.path.isfile(end_info_path)):
-                with open(end_info_path, 'r') as f:
-                    row['end_info'] = OrderedDict(json.load(f))
-                sess_end_time = int(row['end_info']['end_time(ms)'])
-                row['derived_info']['end_time'] = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(sess_end_time/1000))
+            end_info_path = os.path.join(sess_dir, "end_info.json")
+            assert cur_feedback_idx <= len(time2feedback)
+            if os.path.isfile(end_info_path):
+                with open(end_info_path, "r") as f:
+                    row["end_info"] = OrderedDict(json.load(f))
+                sess_end_time = int(row["end_info"]["end_time(ms)"])
+                row["derived_info"]["end_time"] = time.strftime(
+                    "%Y-%m-%d %H:%M:%S", time.localtime(sess_end_time / 1000)
+                )
 
                 # there may be feedbacks in this session despite having seen the last image
                 # the last data point is using info from info.json
                 last_elem = OrderedDict()
-                last_elem['metadata'] = OrderedDict()
+                last_elem["metadata"] = OrderedDict()
                 # emulate the last arrival
-                last_elem['metadata']['arrival_time(ms)'] = sess_end_time
-                last_elem['derived_stats'] = calc_derived_stats(row['end_info'], sess_end_time)
+                last_elem["metadata"]["arrival_time(ms)"] = sess_end_time
+                last_elem["derived_stats"] = calc_derived_stats(
+                    row["end_info"], sess_end_time
+                )
                 per_img.append(last_elem)
 
-                assert(cur_feedback_idx == len(time2feedback))
-            row['positive_ids'] = pos_feedback_set
-            row['negative_ids'] = neg_feedback_set
-            row['per_img'] = per_img
+                assert cur_feedback_idx == len(time2feedback)
+            row["positive_ids"] = pos_feedback_set
+            row["negative_ids"] = neg_feedback_set
+            row["per_img"] = per_img
             rows.append(row)
         return rows
 
--- app/routes.py
+++ app/routes.py
@@ -4,36 +4,43 @@ from flask import redirect, request, render_template, send_from_directory, jsoni
 from app import app
 from app.logs import *
 
-app.config['LOG_FOLDER'] = None
-app.config['CURRENT_SESSION_NAME'] = None
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.config["LOG_FOLDER"] = None
+app.config["CURRENT_SESSION_NAME"] = None
+app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
 
-'''
+"""
 For loading the replay page
-'''
-@app.route('/replay/<int:sess_num>')
+"""
+
+
+@app.route("/replay/<int:sess_num>")
 def replay(sess_num):
-    if (app.config["LOG_FOLDER"] is None):
-        print("No log folder, defaulting to index");
+    if app.config["LOG_FOLDER"] is None:
+        print("No log folder, defaulting to index")
         return index()
 
     rows = process_data(app.config["LOG_FOLDER"])
     # ignore last one since that's only metadata
-    per_img = rows[sess_num]['per_img'][:-1]
-    pos_ids = rows[sess_num]['positive_ids']
-    neg_ids = rows[sess_num]['negative_ids']
+    per_img = rows[sess_num]["per_img"][:-1]
+    pos_ids = rows[sess_num]["positive_ids"]
+    neg_ids = rows[sess_num]["negative_ids"]
     # print(pos_ids);
-    return render_template('replay.html', per_img=per_img, pos_ids=pos_ids, neg_ids=neg_ids)
+    return render_template(
+        "replay.html", per_img=per_img, pos_ids=pos_ids, neg_ids=neg_ids
+    )
 
-'''
+
+"""
 For serving images!
-'''
-@app.route('/replay_img/<path:filename>')
+"""
+
+
+@app.route("/replay_img/<path:filename>")
 def replay_img(filename):
-    return send_from_directory(app.config['LOG_FOLDER'], filename)
+    return send_from_directory(app.config["LOG_FOLDER"], filename)
 
 
-@app.route('/download/<int:sess_num>')
+@app.route("/download/<int:sess_num>")
 def download(sess_num):
     path = "%d/pred.hyperfindsearch" % sess_num
     return send_from_directory(app.config["LOG_FOLDER"], path)
@@ -43,23 +50,26 @@ def get_stat_series(rows):
     stat_series = []
     # at the end of each row, add a zero point
     for row in rows:
-        if (len(row['per_img']) > 0):
-            stat_series.extend(row['per_img'])
+        if len(row["per_img"]) > 0:
+            stat_series.extend(row["per_img"])
             zero_entry = OrderedDict()
-            zero_entry['metadata'] = OrderedDict()
-            zero_entry['metadata']['arrival_time(ms)'] = row['per_img'][-1]['metadata']['arrival_time(ms)']
-            zero_entry['derived_stats'] = OrderedDict()
-            for k in row['per_img'][-1]['derived_stats']:
-                zero_entry['derived_stats'][k] = 0;
+            zero_entry["metadata"] = OrderedDict()
+            zero_entry["metadata"]["arrival_time(ms)"] = row["per_img"][-1]["metadata"][
+                "arrival_time(ms)"
+            ]
+            zero_entry["derived_stats"] = OrderedDict()
+            for k in row["per_img"][-1]["derived_stats"]:
+                zero_entry["derived_stats"][k] = 0
             stat_series.append(zero_entry)
     return stat_series
 
-@app.route('/refresh_plot')
+
+@app.route("/refresh_plot")
 def refresh_plot():
     stat_series = {}
-    if (app.config["LOG_FOLDER"] and os.path.isdir(app.config["LOG_FOLDER"])):
+    if app.config["LOG_FOLDER"] and os.path.isdir(app.config["LOG_FOLDER"]):
         rows = process_data(app.config["LOG_FOLDER"])
-        if (rows is None):
+        if rows is None:
             print(app.config["LOG_FOLDER"], "resulted in none rows")
             return jsonify({})
         stat_series = get_stat_series(rows)
@@ -68,36 +78,47 @@ def refresh_plot():
 
 
 # just update
-@app.route('/update_log_folder/<path:session_name>')
+@app.route("/update_log_folder/<path:session_name>")
 def update_log_folder(session_name):
     print("Updated with ", session_name)
     app.config["CURRENT_SESSION_NAME"] = session_name
-    app.config["LOG_FOLDER"] = os.path.join(app.config['ROOT_FOLDER'], session_name)
+    app.config["LOG_FOLDER"] = os.path.join(app.config["ROOT_FOLDER"], session_name)
     return index()
 
+
 def reset_states():
-    app.config['LOG_FOLDER'] = None
-    app.config['CURRENT_SESSION_NAME'] = None
+    app.config["LOG_FOLDER"] = None
+    app.config["CURRENT_SESSION_NAME"] = None
+
 
 def invalid_session():
-    return app.config['LOG_FOLDER'] is None or app.config['CURRENT_SESSION_NAME'] is None
+    return (
+        app.config["LOG_FOLDER"] is None or app.config["CURRENT_SESSION_NAME"] is None
+    )
 
-@app.route('/')
+
+@app.route("/")
 def index():
     candidates = get_all_files(app.config["ROOT_FOLDER"], app.config["ROOT_FOLDER"])
     if len(candidates) == 0:
-        return render_template('index.html')
+        return render_template("index.html")
 
     print("candidates: ", candidates)
-    if invalid_session() or app.config['CURRENT_SESSION_NAME'] not in candidates:
+    if invalid_session() or app.config["CURRENT_SESSION_NAME"] not in candidates:
         update_log_folder(candidates[0])
 
     rows = None
     stat_keys = None
-    if (app.config["LOG_FOLDER"] and os.path.isdir(app.config["LOG_FOLDER"])):
+    if app.config["LOG_FOLDER"] and os.path.isdir(app.config["LOG_FOLDER"]):
         rows = process_data(app.config["LOG_FOLDER"])
 
-    if rows is not None and len(rows) > 0 and len(rows[0]['per_img']) > 0:
-        stat_keys = rows[0]['per_img'][0]['derived_stats'].keys()
+    if rows is not None and len(rows) > 0 and len(rows[0]["per_img"]) > 0:
+        stat_keys = rows[0]["per_img"][0]["derived_stats"].keys()
 
-    return render_template('index.html', session_name=app.config["CURRENT_SESSION_NAME"], rows=rows, candidate_logs=candidates, stat_keys=stat_keys)
+    return render_template(
+        "index.html",
+        session_name=app.config["CURRENT_SESSION_NAME"],
+        candidate_logs=candidates,
+        rows=rows,
+        stat_keys=stat_keys,
+    )
